96411f2bd8e80f9d1398ed5644da25edf5f5c9f3
Jorge Duque <jor.duqu@gmail.com>
2022-02-16 22:20:24 -0600

    Made this even more haskelly

--- app/Main.hs
+++ app/Main.hs
@@ -1,39 +1,31 @@
 module Main where 
 
 
-import Wordscape (Dictionary, possibleWords, makeDict)
+import Wordscape (Dictionary, Words, possibleWords, makeDict)
+import System.Environment (getArgs)
 import qualified Data.Text.IO as TIO
 import qualified Data.Text as T
 
-loadDictionary :: FilePath -> IO Dictionary
-loadDictionary fp = do
-    text <- TIO.readFile fp
-    let dict = makeDict text
-    return dict
 
--- add functionality to handle comman dine args instead of promptiong user
+getWordscapePossibilities :: String -> FilePath -> IO ()
+getWordscapePossibilities givenChars dictPath = do
+    text <- TIO.readFile dictPath
+    let dictionary = makeDict text
+    let inputChars = T.unpack $ T.toLower $ T.pack givenChars
+    let solutions = possibleWords inputChars dictionary
+    let numSols = length solutions
+    let header = "There are " ++ show numSols ++ " possible solutions:"
+    putStrLn header
+    putStrLn (replicate (length header) '=') 
+    mapM_ putStrLn solutions
+
 
 main :: IO ()
 main = do
-    -- loading dictionary 
-    let dictPath = "/usr/share/dict/american-english"
-    dictionary <- loadDictionary dictPath
-    
-    -- process data
-    putStrLn "Please type the letters involved (without spaces):"
-    givenChars <- getLine 
-    let inputData = T.unpack $ T.toLower $ T.pack givenChars
-
-    if length inputData < 3
-
-        then do
-            putStrLn "Invalid input, need at least three characters"
-
-        else do
-            let solutions = possibleWords inputData dictionary
-            let numSols = length solutions
-            let header = "There are " ++ show numSols ++ " possible solutions:"
-
-            putStrLn header
-            putStrLn (replicate (length header) '=')
-            mapM_ putStrLn solutions
+    args <- getArgs 
+    case args of 
+        [chars] -> getWordscapePossibilities chars "/usr/share/dict/american-english"
+        [chars, "-d", dict] -> getWordscapePossibilities chars dict
+        _ -> putStrLn $ "Usage: will give possible solutions to a wordscape game given a string of characters\n" ++
+                        "       wordscape letters\n" ++
+                        "       wordscape letters -d dictionary"
